46e3f09b657ee84faafa1c88236c43e536cf2cd7
Alexey Panfilov <ap83@list.ru>
2022-10-14 17:43:31 +0300

    Save server to data.bin added

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,7 @@ package org.example;
 
 import org.example.serverlogic.IncomingPurchase;
 import org.example.serverlogic.MaxCategoryCalc;
+import org.example.user.DataSaveAndLoad;
 
 import java.io.*;
 import java.net.ServerSocket;
@@ -12,9 +13,15 @@ public class Main {
     private static final int PORT = 8989;
 
     public static void main(String[] args) {
-        File tsvFile = new File("categories.tsv");
         MaxCategoryCalc max = new MaxCategoryCalc();
-        max.readFromTsv(tsvFile);
+        File binServerStateFile = new File("data.bin");
+        if (binServerStateFile.exists()) {
+            max = DataSaveAndLoad.loadServerStateFromBin(binServerStateFile);
+        } else {
+            System.out.println("Файл покупок не найден, будет формироваться новая статистика");
+        }
+        File tsvCategoriesListFile = new File("categories.tsv");
+        max.readFromTsv(tsvCategoriesListFile);
         try (ServerSocket serverSocket = new ServerSocket(PORT);) { // стартуем сервер один(!) раз
             while (true) { // в цикле(!) принимаем подключения
                 try (
@@ -27,6 +34,8 @@ public class Main {
                     incomingPurchase.splitJson(incomingData);
                     String category = max.assignCategory(incomingPurchase);
                     out.println(max.categoriesCount(category, incomingPurchase.getSum()));
+                    DataSaveAndLoad saveServer = new DataSaveAndLoad();
+                    saveServer.saveServerStatToBin(binServerStateFile, max);
 
                     // обработка одного подключения
                 }
